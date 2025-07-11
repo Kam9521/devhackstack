e936f5e9f20eb09449bfcc0377c8468c1b66f1ff
Kamil <szpmytyl@wp.pl>
2025-07-11 21:18:15 +0200

    Naprawa błędu Link is not defined

--- src/components/Zamow.jsx
+++ src/components/Zamow.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Zamow({ darkMode }) {
   return (
     <div className={'container mt-5 text-$ {darkMode ? "light" : "dark"}'}>
